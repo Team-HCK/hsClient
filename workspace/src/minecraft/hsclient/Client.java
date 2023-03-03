14a3ea0436f428314dcbdf5ef88217e0902ad0b4
songro_ <sdbb2q@gmail.com>
2023-03-03 16:41:03 +0900

    Gui setup

---
+++ workspace/src/minecraft/hsclient/Client.java
@@ -0,0 +1,14 @@
+package hsclient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.lwjgl.opengl.Display;
+
+public class Client {
+
+    public static void startup() {
+        System.out.println("starting hsClient");
+    }
+
+}
---
+++ workspace/src/minecraft/hsclient/ui/MainMenu.java
@@ -0,0 +1,17 @@
+package hsclient.ui;
+
+import hsclient.Client;
+import net.minecraft.client.gui.GuiLanguage;
+import net.minecraft.client.gui.GuiMultiplayer;
+import net.minecraft.client.gui.GuiOptions;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiSelectWorld;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.ResourceLocation;
+
+
+public class MainMenu {
+
+
+
+}
